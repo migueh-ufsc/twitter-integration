b25bf31d4772354ab84dcdd56ddbd409a24a8285
Indiara C. Menegat <indicm@gmail.com>
2022-10-19 01:31:57 -0300

    tweet e user schema e model

---
+++ src/infra/database/schemas/TweetSchema.ts
@@ -0,0 +1,50 @@
+import { ITweet } from 'contracts/entities/ITweet';
+import { model, Schema } from 'mongoose';
+import { schemaOptions } from '../SchemaOptions';
+
+export const TweetSchema = new Schema<ITweet>(
+  {
+    id: {
+      type: String,
+      required: true,
+      index: true,
+    },
+    text: {
+      type: String,
+    },
+    authorId: {
+      type: String,
+    },
+    nRetweet: {
+      type: Number,
+    },
+    nReply: {
+      type: Number,
+    },
+    nLike: {
+      type: Number,
+    },
+    nQuote: {
+      type: Number,
+    },
+    mentions: [
+      {
+        username: {
+          type: String,
+        },
+        id: {
+          type: String,
+        },
+      },
+    ],
+    isReply: {
+      type: Boolean,
+    },
+    geolocation: {
+      type: String,
+    },
+  },
+  schemaOptions,
+);
+
+export const TweetModel = model<ITweet>('Tweet', TweetSchema);
