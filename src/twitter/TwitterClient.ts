68a469f7f797b378ae1c9e515e699fb50e9182c8
indistormborn <indicm@gmail.com>
2022-10-21 03:32:15 -0300

    twitter client busca user por id e por username

--- src/twitter/TwitterClient.ts
+++ src/twitter/TwitterClient.ts
@@ -1,6 +1,33 @@
+import { User } from 'entities/User';
 import { Config } from 'infra/config';
 import { TwitterApi } from 'twitter-api-v2';
+import { userParams } from './helpers/Params';
+import { twitterUserToUser } from './helpers/Parsers';
 
 export class TwitterClient {
   private static readonly client = new TwitterApi(Config.twitter.bearerToken);
+  private static v2 = TwitterClient.client.v2;
+
+  public static async getUserById(id: string): Promise<User> {
+    try {
+      const twitterUser = await TwitterClient.v2.user(id, userParams);
+      return twitterUserToUser(twitterUser);
+    } catch (err: unknown) {
+      throw err;
+      //todo melhorar logs aqui
+    }
+  }
+
+  public static async getUserByUsername(username: string): Promise<User> {
+    try {
+      const twitterUser = await TwitterClient.v2.userByUsername(
+        username,
+        userParams,
+      );
+      return twitterUserToUser(twitterUser);
+    } catch (err: unknown) {
+      throw err;
+      //todo melhorar logs aqui
+    }
+  }
 }
