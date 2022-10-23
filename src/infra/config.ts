6c634fecbf2a4e1ade3bb04a19b98137b2df3429
indistormborn <indicm@gmail.com>
2022-10-23 01:36:54 -0300

    parametros de pesquisa de tweet

--- src/infra/config.ts
+++ src/infra/config.ts
@@ -20,5 +20,9 @@ export abstract class Config {
     token: process.env.TWITTER_ACCESS_TOKEN,
     tokenSecret: process.env.TWITTER_ACCESS_SECRET,
     bearerToken: process.env.TWITTER_BEARER_TOKEN,
+    maxResults:
+      process.env.TWITTER_TIMELINE_MAX_RESULTS != null
+        ? Number(process.env.TWITTER_TIMELINE_MAX_RESULTS)
+        : 30,
   };
 }
--- src/twitter/helpers/Params.ts
+++ src/twitter/helpers/Params.ts
@@ -1,4 +1,5 @@
-import { UsersV2Params } from 'twitter-api-v2';
+import { Config } from 'infra/config';
+import { TweetV2UserTimelineParams, UsersV2Params } from 'twitter-api-v2';
 
 export const userParams: Partial<UsersV2Params> = {
   'user.fields': [
@@ -11,5 +12,18 @@ export const userParams: Partial<UsersV2Params> = {
     'public_metrics',
     'username',
     'verified',
+  ],
+};
+
+export const timelineParams: Partial<TweetV2UserTimelineParams> = {
+  max_results: Config.twitter.maxResults,
+  'tweet.fields': [
+    'author_id',
+    'id',
+    'in_reply_to_user_id',
+    'entities',
+    'public_metrics',
+    'text',
+    'referenced_tweets',
   ],
 };
