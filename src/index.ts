d11c1e9b5da9069bca978d9e1bfe2599561b02a1
indistormborn <indicm@gmail.com>
2022-10-23 19:31:01 -0300

    limpando index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,7 @@
 import { default as Database } from 'infra/database/Connection';
 import { default as Broker } from 'infra/message-broker/Manager';
 import { default as Server } from 'infra/server/Server';
-import { TweetService } from 'services/TweetService';
-import { UserService } from 'services/UserService';
-import { GetUserUseCase } from 'usecases/GetUserUseCase';
 
 (async () => {
-  Promise.all([Database.init(), Broker.init(), Server.init()]).then(
-    async () => {
-      const userService = new UserService();
-      const tweetService = new TweetService();
-      const usecase = new GetUserUseCase(userService, tweetService);
-
-      await usecase.execute({
-        username: 'blackfyre_',
-      });
-    },
-  );
+  Promise.all([Database.init(), Broker.init(), Server.init()]);
 })();
